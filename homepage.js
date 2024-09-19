5fa81ac1e3f79a3c221eac7bb044a0732b2a90cd
bunnitaa <susanyem50@gmail.com>
2024-09-19 16:59:58 -0400

    Initial commit

--- homepage.js
+++
@@ -1,9 +0,0 @@
-// Cake Crumbs Website - Basic JavaScript File
-console.log("Welcome to Cake Crumbs!");
-
-function greetUser(username) {
-    return `Hello, ${username}! Enjoy browsing Cake Crumbs.`;
-}
-
-let user = "Customer";
-console.log(greetUser(user));
